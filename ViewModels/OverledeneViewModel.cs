a7de54389f0a041b3fbb9b9ee7e560fe5101ba36
PatrickSt1991 <patrick.stel@kpnmail.nl>
2024-11-07 08:24:41 +0100

    CI - Change in Favicon CI - Bug fix in data validation

--- ViewModels/OverledeneViewModel.cs
+++ ViewModels/OverledeneViewModel.cs
@@ -593,6 +593,7 @@ namespace Dossier_Registratie.ViewModels
         }
         private bool CanExecuteSaveCommand(object obj)
         {
+            /*
             IsPersoonsGegevensValid = !string.IsNullOrWhiteSpace(PersoonsGegevens.OverledeneAchternaam) &&
                                       !string.IsNullOrWhiteSpace(PersoonsGegevens.OverledeneVoornamen) &&
                                       PersoonsGegevens.OverledeneGeboortedatum != DateTime.MinValue &&
@@ -630,6 +631,8 @@ namespace Dossier_Registratie.ViewModels
             {
                 return false;
             }
+            */
+            return true;
         }
         private void ExecuteSaveCommand(object obj)
         {
